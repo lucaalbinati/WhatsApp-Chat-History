1291cf920c4fb73a281f4546eda999c3721da123
Luca Albinati <luca.albinati@gmail.com>
2021-07-18 11:55:17 +0200

    extract sender from message

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ import collections
 
 DATE_MESSAGE_SEPARATOR = " - "
 DATE_REGEX = r"^([0]{0,1}[1-9]|1[012])\/([1-9]|([012][0-9])|(3[01]))\/\d\d,\s([0-1]?[0-9]|2?[0-3]):([0-5]\d)$"
+SENDER_MESSAGE_SEPARATOR = ": "
 
 def convert_file_to_list(path):
+	# return list of tuple: (date, sender, message)
+
 	f = open(path, 'r')
 	lines = f.readlines()
 
 	dated_messages = []
 
 	curr_date = ""
+	curr_sender = ""
 	curr_message = ""
 
 	for line in lines:
@@ -25,11 +29,16 @@ def convert_file_to_list(path):
 			if re.match(DATE_REGEX, potential_date):
 				if curr_date != "":
 					# on the first line, 'curr_date' will be equal to "", so we don't append anything
-					dated_messages.append((curr_date, curr_message))
+					dated_messages.append((curr_date, curr_sender, curr_message))
+
+				sender_message_begin_index = date_message_separator_index + len(DATE_MESSAGE_SEPARATOR)
+				sender_message = line[sender_message_begin_index:]
+				sender_end_index = sender_message.index(SENDER_MESSAGE_SEPARATOR)
+				message_begin_index = sender_end_index + len(SENDER_MESSAGE_SEPARATOR)
 
 				curr_date = potential_date
-				message_begin_index = date_message_separator_index + len(DATE_MESSAGE_SEPARATOR)
-				curr_message = line[message_begin_index:]
+				curr_sender = sender_message[:sender_end_index]
+				curr_message = sender_message[message_begin_index:]
 			else:
 				curr_message += line
 		except ValueError as e:
@@ -48,6 +57,8 @@ def convert_dates(dated_messages):
 	return dated_messages
 
 def amount_per_month(dated_messages):
+	# return dict with key=(month,year) and value=amount
+
 	nb_per_month = {}
 	for (date, _) in dated_messages:
 		month_year = datetime(year=date.year, month=date.month, day=1)
@@ -76,6 +87,12 @@ def plot_month_frequency(nb_per_month):
 	plt.xticks(rotation=65)
 	plt.plot(xaxis, values)
 	plt.show()
+
+def print_dated_messages(dated_messages):
+	for (date, sender, msg) in dated_messages:
+		print(date)
+		print(sender)
+		print(msg)
 
 def main():
 	dated_messages = convert_file_to_list("data/Neuer.txt")
